6fa2dcba47a3ff3b1b800844ddc29124f404aa57
mostdota <marcus.v.n.f.filho@gmail.com>
2026-03-09 23:25:22 -0300

    Fix typo in axios import statement

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 // ============================================================
 
 const express = require("express");
-const axios = require("axios");h
+const axios = require("axios");
 const app = express();
 app.use(express.json());
 
